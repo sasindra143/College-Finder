fdd44f99cd2537dee2d0f250f35fc8888537b8c6
sasindra143 <your-sasindragandla@gmail.com>
2026-05-04 00:51:22 +0530

    fix: revert to purely app router error pages with force-dynamic to bypass netlify static generation issues

--- frontend/src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from 'next/app';
-
-// This is the Pages Router _app.tsx.
-// It is completely separate from the App Router layout.
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
