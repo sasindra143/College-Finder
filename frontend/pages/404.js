53147914e3727f193914ff8f00a4c20afb6765ff
sasindra143 <your-sasindragandla@gmail.com>
2026-05-04 01:05:02 +0530

    fix: commented out netlify plugin and removed pages dir to try pure netlify build process

--- frontend/pages/404.js
+++
@@ -1,9 +0,0 @@
-export default function Custom404() {
-  return (
-    <div style={{ textAlign: 'center', padding: '100px 20px', fontFamily: 'sans-serif' }}>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-      <a href="/" style={{ color: 'blue', textDecoration: 'underline' }}>Go back home</a>
-    </div>
-  );
-}
--- frontend/pages/500.js
+++
@@ -1,9 +0,0 @@
-export default function Custom500() {
-  return (
-    <div style={{ textAlign: 'center', padding: '100px 20px', fontFamily: 'sans-serif' }}>
-      <h1>500 - Server Error</h1>
-      <p>An unexpected error occurred.</p>
-      <a href="/" style={{ color: 'blue', textDecoration: 'underline' }}>Go back home</a>
-    </div>
-  );
-}
